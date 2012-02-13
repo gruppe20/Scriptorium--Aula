696efd01b40c6f2f82ed6bad68003fd84328351c
Ole Grinnby <ogrinnby@gmail.com>
2012-02-13 13:41:57 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aula::Application.config.secret_token = '38c70f948e81394c21280832280caf524d215eda51015c7236a86c4b8675ff4296c7e235bb3c0e057f933fb82862e6e8dfaf63762023105cb001649170b29543'
